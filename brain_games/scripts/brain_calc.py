984831763e180f21bcecb1b2ed380b05fae11e13
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2024-11-10 05:02:48 +0400

    feat: add calculate-game

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,45 @@
+import random
+import prompt
+from brain_games.cli import name
+
+
+def operate(a, b, operation):
+    match operation:
+        case 0:
+            return a + b
+        case 1:
+            return a - b
+        case 2:
+            return a * b
+
+
+def get_symbol(operation):
+    match operation:
+        case 0:
+            return '+'
+        case 1:
+            return '-'
+        case 2:
+            return '*'
+
+
+def calc():
+    attempts = 3
+    print('What is the result of the expression?')
+
+    for _ in range(attempts):
+        number1 = random.randint(1, 99)
+        number2 = random.randint(1, 99)
+        operation = random.randint(0, 2)
+        result = operate(number1, number2, operation)
+        print(f"Question {number1} {get_symbol(operation)} {number2}")
+        answer = prompt.string("Your answer: ")
+
+        if answer != result.__str__():
+            print(f"'{answer}' is wrong answer ;(."
+                  f" Correct answer was '{result}'")
+            return
+
+        print('Correct!')
+
+    print(f'Congratulations, {name}!')
