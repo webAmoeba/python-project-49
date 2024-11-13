22278a3b0a0d48129cf5a71bbaf9c7f32681113a
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2024-11-13 11:41:10 +0400

    fix: correct brain-gcd game

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -1,3 +1,4 @@
+import math
 from brain_games.scripts.template import play_game, get_random_number
 
 
@@ -9,9 +10,11 @@ def main():
 def generate_question_and_answer():
     number1 = get_random_number(1, 9)
     number2 = get_random_number(1, 9)
-    result = get_random_number(1, 9)
+    divisor = get_random_number(1, 9)
 
-    number1 *= result
-    number2 *= result
+    number1 *= divisor
+    number2 *= divisor
+
+    result = math.gcd(number1, number2)
 
     return f"Question: {number1} {number2}", result.__str__()
